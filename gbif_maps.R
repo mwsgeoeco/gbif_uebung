509fcadcee8b845d7a27d52e98aedea46fb9fe2d
mwsgeoeco <104362966+mwsgeoeco@users.noreply.github.com>
2022-12-14 11:10:26 +0100

    Add files via upload
    Datei angepasst und Beispiele für Borstgrasrasen, Schlickgras-Gesellschaften und Schwermetall-Grasnelken-Gesellschaft eingefügt

--- gbif_maps.R
+++ gbif_maps.R
@@ -10,67 +10,67 @@ library(mapview)
 
 # Beispiel# Schlickgras-Gesellschaften
 # Kennarten: "Spartina maritima", "Spartina anglica", "Spartina townsendii"
-
 speclist <- c("Spartina maritima", "Spartina anglica", "Spartina townsendii")
 
+# Beispiel Borstgrasrasen /entfernt: "Holcus lanatus", "Avenella flexuosa", "Festuca ovina"
+speclist <- c("Nardus stricta", "Danthonia decumbens", "Potentilla erecta", "Carex pilulifera", "Luzula campestris", "Antennaria dioica") 
 
-keys <- vapply(speclist, function(x) name_suggest(x)$data$key[1], 
-               numeric(1), USE.NAMES=FALSE)
+#  diese Funktion soll dabei helfen die richtige Art zu finden
+keys <- vapply(speclist, function(x) name_suggest(x)$data$key[1], numeric(1), USE.NAMES=FALSE)
+
+
+# Beispiel Schwermetall
+speclist <- c("Minuartia caespitosa", "Silene vulgaris subsp. vulgaris", "Armeria maritima") 
+# können nicht mit der Funktion automatisiert ausgelesen werden, da es Armeria maritima von zwei Autoren gibt. Daher lieber gleich die Keys angeben
+keys <- c("7509175", "5587240","8930531")
 
 
 
-schlickgras <- occ_data(taxonKey  = keys, country="DE", hasCoordinate = TRUE, limit = 20000, datasetKey="e6fab7b3-c733-40b9-8df3-2a03e49532c1")
+# auslesen der Daten aus GBIF. Für Borstgrasrasen dauert das sehr lange. 
+gbif_species <- occ_data(taxonKey =keys, country="DE", hasCoordinate = TRUE, limit = 30000, datasetKey="e6fab7b3-c733-40b9-8df3-2a03e49532c1")
 
-schlickgras # kompliziertes Format
+# Aus GBIF wird eine verschachtelte Liste exportiert, mit dieser Funktion macht man einen Data-Frame draus. 
+gbif_species_df<- rbindlist(lapply(gbif_species, function(x) x$data), fill = TRUE, use.names = TRUE)
 
-schlickgras_df<- rbindlist(lapply(schlickgras, function(x) x$data), fill = TRUE, use.names = TRUE)
+# auswählen von Koordinaten und Artname
+gbif_species_df <- gbif_species_df[,c("species","decimalLatitude","decimalLongitude")]
 
-schlickgras_df <- schlickgras_df[,c("scientificName","decimalLatitude","decimalLongitude")]
-
-table(schlickgras_df$scientificName)
+# anzeigen, wie viele Vorkommen gefunden wurden
+table(gbif_species_df$species)
 
 # Doppelungen löschen
-schlickgras_df <- schlickgras_df[!duplicated(schlickgras_df),]
+gbif_species_df <- gbif_species_df[!duplicated(gbif_species_df),]
 
 
+###########################################################################
+#################### Zählen der Kombination und plotten auf Karte #########
 
-######################################################
-# 2. Anschauen der einzelnen Ergebnisse auf der Karte 
-######################################################
+# Umwandeln in SimpleFeature Geodaten
 
-
-# Umwandeln in SimpleFeature
-
-schlickgras_df_geo <- st_as_sf(schlickgras_df, coords = c("decimalLongitude", "decimalLatitude"), crs = st_crs(4326))
-
-# plotten der einzelnen Arten
-unique(schlickgras_df_geo$scientificName)
-
-plot(schlickgras_df_geo[schlickgras_df_geo$scientificName=="Spartina anglica C.E.Hubb.",])
-plot(schlickgras_df_geo[schlickgras_df_geo$scientificName=="Spartina ×townsendii H.Groves & J.Groves",])
-
-# auf Karte plotten
-mapview(schlickgras_df_geo[schlickgras_df_geo$scientificName=="Spartina anglica C.E.Hubb.",], map.types="OpenStreetMap")
-mapview(schlickgras_df_geo[schlickgras_df_geo$scientificName=="Spartina ×townsendii H.Groves & J.Groves",], map.types="OpenStreetMap")
-
-
-
-
-#####################################################
-#################### Zählen der Kombination #########
+gbif_species_df_geo <- st_as_sf(gbif_species_df, coords = c("decimalLongitude", "decimalLatitude"), crs = st_crs(4326))
 
 # schreiben der Koordinaten in einen Data Frame
-df <- st_coordinates(schlickgras_df_geo)
+df <- st_coordinates(gbif_species_df_geo)
 df <- as.data.frame(df)
 
 # zählen der Doppelungen
 df2 <-  setDT(df)[,list(value=.N),names(df)]
 df2 <- as.data.frame(df2)
 
-# plotten der Anzahl
-schlickgras_count <- st_as_sf(df2, coords = c("X", "Y"), crs = st_crs(4326))
+table(df2$value)
 
-mapview(schlickgras_count, map.types="OpenStreetMap")
+# Anzeigen aller Arten der Pflanzengesellschaft, oder fast alle Arten (Anzahl-1)
+df2 <- df2[df2$value>=(max(df2$value)-1),] 
+df2$value <- as.factor(df2$value)
+
+# plotten der Anzahl
+df_count <- st_as_sf(df2, coords = c("X", "Y"), crs = st_crs(4326))
+
+mapview(df_count, map.types="OpenStreetMap", cex = 2 )
+
+
+####
+
 
 
 
